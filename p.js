d4e4ff7413239bf5b717d420733800807fac741e
maulana <maulana.alirridlo@gmail.com>
2020-01-17 21:37:20 +0700

    5

--- p.js
+++ p.js
@@ -1,4 +1,4 @@
-function hasil(){
+function tambah(){
 		var i=parseInt(document.getElementById('bil1').value);
 		var j=parseInt(document.getElementById('bil2').value);
 		if (isNaN(i) ||  isNaN(j)) {
@@ -9,4 +9,40 @@ function hasil(){
 		alert("hasilnya adalah "+jumlah)
 		return jumlah
 		}
-	}
+}
+function kurangi(){
+		var i=parseInt(document.getElementById('bil1').value);
+		var j=parseInt(document.getElementById('bil2').value);
+		if (isNaN(i) ||  isNaN(j)) {
+  		alert("mohon isi angka");
+  		}
+		else{
+		var kurang=i-j
+		alert("hasilnya adalah "+kurang)
+		return kurang
+		}
+}
+function kali(){
+		var i=parseInt(document.getElementById('bil1').value);
+		var j=parseInt(document.getElementById('bil2').value);
+		if (isNaN(i) ||  isNaN(j)) {
+  		alert("mohon isi angka");
+  		}
+		else{
+		var kali=i*j
+		alert("hasilnya adalah "+kali)
+		return kali
+		}
+}
+function bagi(){
+		var i=parseInt(document.getElementById('bil1').value);
+		var j=parseInt(document.getElementById('bil2').value);
+		if (isNaN(i) ||  isNaN(j)) {
+  		alert("mohon isi angka");
+  		}
+		else{
+		var bagi=i/j
+		alert("hasilnya adalah "+bagi)
+		return bagi
+		}
+}
